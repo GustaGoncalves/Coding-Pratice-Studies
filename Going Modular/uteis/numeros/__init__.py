dc590243374cdc560aaf510c9d29f0d13b48de1d
Gustavo Nascimento <167471662+GustaGoncalves@users.noreply.github.com>
2024-07-02 22:09:19 -0300

    Create __init__.py

---
+++ Going Modular/uteis/numeros/__init__.py
@@ -0,0 +1,16 @@
+def fatorial(num):
+ fat=1
+ for i in range(num,0,-1):
+  fat*=i
+ return(fat)
+
+
+def dobro(num):
+ dobre=num*2
+ return(dobre)
+
+
+def triplo(num):
+ triple=num*3
+ return(triple)
+  
